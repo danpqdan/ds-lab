883db08df49dab6d1f689ba5e1f6f0414b1f5fa1
Daniel Santos <danieltisantos@gmail.com>
2022-10-15 16:15:21 -0300

    Primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles/global.css";
+
+export function App() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold">Hellow world</h1>
+
+      <button className="bg-cyan-300 font-medium px-4 py-2 rounded text-white hover:bg-cyan-500">Botão</button>
+    </>
+  )
+}
